771a362346d278a9985ab441e24197af850330ff
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-22 23:59:55 +0700

    update code.

--- app/src/main/java/com/tweentyscoops/mvvm/api/BaseSubscribe.kt
+++ app/src/main/java/com/tweentyscoops/mvvm/api/BaseSubscribe.kt
@@ -4,14 +4,14 @@ import io.reactivex.subscribers.DefaultSubscriber
 
 class BaseSubscribe<T> : DefaultSubscriber<T>() {
     override fun onComplete() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onError(t: Throwable?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onNext(t: T) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
---
+++ app/src/main/java/com/tweentyscoops/mvvm/di/RetrofitModule.kt
@@ -0,0 +1,13 @@
+package com.tweentyscoops.mvvm.di
+
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RetrofitModule {
+
+    @Provides
+    @Singleton
+
+}
